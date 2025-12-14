20123f610d0cfb733e881976a077970ea4ab9efc
maxim.f <maxim.f@rocketfirm.com>
2025-12-14 14:39:28 +0700

    - Normalize track codes by removing brackets, tabs, and spaces in `TracksImport`. - Add a new `newstock` Blade view with a dynamic track code uploader. - Update navigation image with `newchina.jpeg`. - Extend `DashboardController` to support the `newstock` user type.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -48,8 +48,6 @@
                             <p class="mb-6 text-sm text-dark font-bold">Адрес склада в Китае</p>
                             <p class="mb-6 text-sm text-dark" id="china">@yield( 'chinaaddress' ) <br />
                                 <b>({{ \Illuminate\Support\Facades\Auth::user()->login }}){{ \Illuminate\Support\Facades\Auth::user()->name }} </b><br />
-                                收货人: <b>@yield( 'title_text' )({{ Auth::user()->city }})</b> <br />
-                                电话: <b>@yield( 'address_two' )</b>
                             </p>
                             <p class="mb-6 text-sm text-dark" style="display: none;" id="chinaaddress">@yield( 'address_two' )@yield( 'chinaaddress' )</p>
                             <button onclick="copyText()" class="focus:outline-none text-white bg-[#b6da3a] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Копировать</button>
