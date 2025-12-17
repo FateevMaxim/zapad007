9b8df6ddf549e9facca46f0277720b9d4cc708cb
FateevMaxim <fateev.maxim@gmail.com>
2025-12-17 12:38:45 +0500

    import update

--- app/Imports/TracksImport.php
+++ app/Imports/TracksImport.php
@@ -8,17 +8,15 @@ use Maatwebsite\Excel\Concerns\SkipsOnError;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
 
-class TracksImport implements ToModel, SkipsOnError, WithCustomCsvSettings
+class TracksImport implements ToModel, SkipsOnError
 {
 
     use Importable;
     private $date;
-    private string $delimiter;
 
-    public function __construct(string $date, string $delimiter = ',')
+    public function __construct(string $date)
     {
         $this->date = $date;
-        $this->delimiter = $delimiter;
     }
     /**
      * @param \Throwable $e
@@ -28,41 +26,17 @@ class TracksImport implements ToModel, SkipsOnError, WithCustomCsvSettings
         // Handle the exception how you'd like.
     }
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
-        // Предохранитель: если строка пустая
-        if (!isset($row[0])) {
-            return null;
-        }
-
-        // Удаляем квадратные скобки, табы и пробелы
-        $trackCode = preg_replace('/[\[\]\s\t]/', '', $row[0]);
-
-        if ($trackCode === '') {
-            return null;
-        }
-
         return new TrackList([
-            'track_code' => $trackCode,
+            'track_code' => $row[0],
             'to_china' => $this->date,
             'status' => 'Получено в Китае',
             'reg_china' => 1,
-            'created_at' => date(now()),
         ]);
-    }
-
-    public function getCsvSettings(): array
-    {
-        return [
-            // Разделитель задаётся извне (автоопределение в контроллере), кодировка UTF-8
-            'delimiter' => $this->delimiter,
-            'enclosure' => '"',
-            'escape' => '\\',
-            'input_encoding' => 'UTF-8',
-        ];
     }
 }
